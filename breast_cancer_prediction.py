3cd7a4afc8997d627d786f8c30bef4d35624e2b6
Adarsh Nashine <136648774+adarsh-n2003@users.noreply.github.com>
2024-04-17 01:51:59 +0530

    Update breast_cancer_prediction.py

--- breast_cancer_prediction.py
+++ breast_cancer_prediction.py
@@ -4,14 +4,11 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 
-# Get the path to the current directory
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Load the trained model
 model_path = os.path.join(current_directory, 'bcd_model.h5')
 model = load_model(model_path)
 
-# Define allowed extensions for file upload
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 def allowed_file(filename):
@@ -20,21 +17,17 @@ def allowed_file(filename):
 def main():
     st.title('Breast Cancer Detection')
     
-    # File upload
     uploaded_file = st.file_uploader("Choose an image...", type=['png', 'jpg', 'jpeg'])
 
     if uploaded_file is not None:
         if allowed_file(uploaded_file.name):
-            # Preprocess the image
             img = load_img(uploaded_file, target_size=(150, 150))
             img = img_to_array(img)
             img = img.reshape(1, img.shape[0], img.shape[1], img.shape[2])
             img /= 255.
 
-            # Make prediction
             prediction = model.predict(img)
 
-            # Interpret the prediction
             if prediction < 0.5:
                 result = 'Normal'
                 color = 'green'
@@ -45,7 +38,6 @@ def main():
                 result = 'Unpredictable'
                 color = 'orange'
 
-            # Display result with highlight
             st.write(f'Prediction: <span style="color:{color}">{result}</span>', unsafe_allow_html=True)
         else:
             st.error("Invalid file format. Please upload an image file.")
